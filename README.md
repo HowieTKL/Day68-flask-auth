4c4ed64593aaca83deb22f20bcd78e541081b594
HowieTKL <10776992+HowieTKL@users.noreply.github.com>
2026-03-04 04:40:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A minimal project showcasing password based authentication in Python. Utilizes flask-login session management, and werkzeug password hashing. Day 68 course in [100 days of Code: The Complete Python Pro Bootcamp](https://www.udemy.com/course/100-days-of-code).
---
+++ main.py
@@ -0,0 +1,103 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from werkzeug.security import generate_password_hash, check_password_hash
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+# CREATE TABLE IN DB
+class User(UserMixin, db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(1000))
+
+with app.app_context():
+    db.create_all()
+
+@login_manager.user_loader
+def load_user(user_id):
+    result = db.session.execute(db.select(User).where(User.id == user_id))
+    user = result.scalar()
+    print(user_id, user.name)
+    return user
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+        if user:
+            flash("Email already registered, login instead.")
+            return redirect(url_for("login"))
+
+        new_user = User(email=request.form['email'],
+                        password=generate_password_hash(request.form['password'], salt_length=8),
+                        name=request.form['name'])
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("secrets", name=new_user.name))
+    return render_template("register.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("secrets"))
+    if request.method == 'POST':
+        result = db.session.execute(db.select(User).where(User.email == request.form['email']))
+        user = result.scalar()
+        if check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            print("login success")
+            return redirect(url_for("secrets"))
+        else:
+            error = "Invalid Credentials - please try again."
+            return render_template("login.html", error=error)
+    return render_template("login.html")
+
+@app.route('/secrets')
+@login_required
+def secrets():
+    return render_template("secrets.html", name=current_user.name)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
+@app.route('/download')
+@login_required
+def download():
+    return send_from_directory('static/files', 'cheat_sheet.pdf', as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
